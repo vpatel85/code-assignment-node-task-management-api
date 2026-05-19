b60fde17350f1ede21bff0170efee3db2aca196d
Daemon <daemon@daemon.build>
2026-05-19 14:46:19 +0000

    Add class-validator and class-transformer to enforce DTO validation at runtime

--- test/tasks.e2e-spec.ts
+++ test/tasks.e2e-spec.ts
@@ -14,6 +14,7 @@ describe('TasksController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({
       whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
     }));
     await app.init();
